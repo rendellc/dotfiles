d7d050086e2e6be10c5b3f70fb97491afec1a620
Rendell Cale <rendell.cale@gmail.com>
2023-10-03 19:00:03 +0200

    Update cmp to use new api

--- nvim/config/nvim/after/plugin/lsp.lua
+++ nvim/config/nvim/after/plugin/lsp.lua
@@ -1,4 +1,5 @@
 local lsp = require('lsp-zero')
+local cmp = require('cmp')
 
 lsp.preset('recommended')
 
@@ -11,27 +12,28 @@ vim.diagnostic.config({
 	float = true,
 })
 
-
-local cmp = require('cmp')
-local cmp_select = {behaviour = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-Space>'] = cmp.mapping.complete(),
+local cmp_action = lsp.cmp_action()
+cmp.setup({
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-n>'] = cmp_action.luasnip_jump_forward(),
+		['<C-p>'] = cmp_action.luasnip_jump_backward(),
+		['<C-u>'] = cmp.mapping.scroll_docs(-4),
+		['<C-d>'] = cmp.mapping.scroll_docs(4),
+	})
 })
 
 lsp.set_preferences({
 	sign_icons = {}
 })
 
-lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
-})
-
 lsp.on_attach(function(client, bufnr)
 	local opts = { buffer = bufnr, remap = false}
-	
+
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
