b81daa88283e16ab5366ff59638482d42490398e
rendellc <rendell.cale@gmail.com>
2023-10-03 20:08:44 +0200

    Remove undofiles

--- nvim/config/nvim/lua/rendellc/set.lua
+++ nvim/config/nvim/lua/rendellc/set.lua
@@ -10,8 +10,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("localappdata") .. "/nvim-data/undodir"
-vim.opt.undofile = true
+vim.opt.undofile = false
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
